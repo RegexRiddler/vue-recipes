1958ed9bc07633e7e6b28b798524d2ee4c710098
Maximilian Dybvik <maximilian.dybvik@gmail.com>
2019-03-08 22:34:54 +0100

    Initial commit of working prototype

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,57 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Recipes 
+      :thumbnail="recipes[index].thumbnail"
+      :title="recipes[index].title"
+      :ingredients="recipes[index].ingredients"
+      :url="recipes[index].href"
+    />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Recipes from './components/Recipes.vue'
+import { constants } from 'fs';
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    Recipes
+  },
+  props: {
+
+  },
+  data() {
+    return {
+      recipes: [],
+      index: 0
+    }
+  },
+  beforeMount: function () {
+    const conversionUrl = "https://corsanywhereproxy.herokuapp.com/";
+    const recipeApi = "http://www.recipepuppy.com/api/";
+
+    fetch(conversionUrl + recipeApi)
+      .then(res => res.json())
+      .then(data =>{
+        this.recipes = data.results;
+      })
   }
 }
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style lang="sass">
+  *
+    box-sizing: border-box
+    
+  body
+   background-color: #2A5362
+
+  #app
+    font-family: 'Avenir', Helvetica, Arial, sans-serif
+    -webkit-font-smoothing: antialiased
+    -moz-osx-font-smoothing: grayscale
+    text-align: center
+    color: #1F1F1F
+    margin-top: 60px
 </style>
---
+++ src/components/Recipes.vue
@@ -0,0 +1,56 @@
+<template>
+  <div class="container">
+    <h1>{{ title }}</h1>
+    <img :src="thumbnail" :key="thumbnail" alt="Recipe image">
+    <h5>{{ ingredients }}</h5>
+    <a :href="url">Go to Intructions</a>
+    <button @click="changeRecipe(-1)">Previous</button>
+    <button @click="changeRecipe(1)">Next</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Recipes',
+  props: [
+    'thumbnail', 'title', 'url', 'ingredients'
+  ],
+  methods: {
+    changeRecipe: function (param) {
+      this.$parent.index += param;
+      if (this.$parent.index === this.$parent.recipes.length) {
+        this.$parent.index = 0;
+      } else if (this.$parent.index === -1) {
+        this.$parent.index = this.$parent.recipes.length - 1;
+      }
+    }
+  }
+}
+</script>
+
+<style lang="sass" scoped>
+  .container
+    position: absolute
+    top: 50%
+    left: 50%
+    transform: translate(-50%, -50%)
+    max-width: 600px
+    width: 80%
+    border-radius: .5rem
+    box-shadow: 0 19px 38px rgba(0, 0, 0, 0.30), 0 15px 12px rgba(0, 0, 0, 0.22)
+    background-color: #EAEAEA
+
+  h3
+    margin: 40px 0 0
+
+  ul
+    list-style-type: none
+    padding: 0
+
+  li
+    display: inline-block
+    margin: 0 10px
+
+  a
+    color: #42b983
+</style>
